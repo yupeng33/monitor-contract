bf2ff8da788a0c1fac2253965950e6511868c089
peng <1002371595@qq.com>
2024-07-25 23:15:04 +0800

    更新数据库和redis依赖

--- src/main/java/com/monitor/contract/config/RedissionConfig.java
+++ src/main/java/com/monitor/contract/config/RedissionConfig.java
@@ -1,66 +1,67 @@
 package com.monitor.contract.config;
 
-import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.Resource;
-import java.util.List;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * redission配置
- * 
+ *
  * @author wangjupeng06199
  * @date 2022年1月5日
  */
+@Slf4j
 @Configuration
-@EnableConfigurationProperties(RedissionConfigProperties.class)
 public class RedissionConfig {
 	/**
 	 * 日志
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(RedissionConfig.class);
 	/**
-	 * redis配置属性
-	 */
-	@Resource
-	private RedissionConfigProperties configProperties;
-	/**
 	 * redis配置前缀
 	 */
 	private static final String REDISSON_PREFIX = "redis://";
 
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.database}")
+    private int database;
+
 	/**
 	 * redission客户端
-	 * 
+	 *
 	 * @return
 	 */
 	@Bean
 	public RedissonClient redissonClient() {
 		Config config = new Config();
-		List<String> redissonNodes = configProperties.getCluster().getNodes();
-		String[] urls = new String[redissonNodes.size()];
-		for (int i = 0; i < redissonNodes.size(); i++) {
-			urls[i] = REDISSON_PREFIX + redissonNodes.get(i);
-		}
-		String password = configProperties.getPassword();
-		config.useClusterServers().addNodeAddress(urls).setPassword(password).setConnectTimeout(60000)
-				.setPingConnectionInterval(600000).setTimeout(60000);
-		String urlStr = JSON.toJSONString(urls);
+        config.useSingleServer().setAddress(REDISSON_PREFIX + host + ":" + port).setDatabase(database);
 		try {
-
-			LOGGER.info("RedissonClient init redis url:[{}] successed.", urlStr);
-			return Redisson.create(config);
-		} catch (Exception e) {
-			String errorMsg = String.format("RedissonClient init redis url:[{}], Exception:", urlStr);
-			LOGGER.error(errorMsg, e);
+            RedissonClient redissonClient = Redisson.create(config);
+            log.info("Init redissonClient success");
+            return redissonClient;
+        } catch (Exception e) {
+			LOGGER.error("RedissonClient init redis error, Exception:\n", e);
 			return null;
 		}
 	}
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(connectionFactory);
+        return stringRedisTemplate;
+    }
 }
